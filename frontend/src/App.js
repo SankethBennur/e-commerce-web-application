436325db46dd416649780df2c904a75a413f9dc6
SankethBennur <snkthbnnr3@gmail.com>
2021-12-12 22:37:33 +0530

    Using my frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,51 +1,22 @@
-import React, { useEffect } from 'react';
-import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import Home from './containers/Home';
-import Signin from './containers/Signin';
-import Signup from './containers/Signup';
-import PrivateRoute from './components/HOC/PrivateRoute';
-import { useDispatch, useSelector } from 'react-redux';
-import { isUserLoggedIn, getInitialData } from './actions';
-import Products from './containers/Products';
-import Orders from './containers/Orders';
-import Category from './containers/Category';
-import NewPage from './containers/NewPage';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Header from './components/layouts/layout.header.js';
+import Home from './containers/containers.home';
+import Signin from './containers/containers.signin';
+import Signup from './containers/containers.signup';
 
-
-
-function App() {
-
-  const dispatch = useDispatch();
-  const auth = useSelector(state => state.auth)
-
-
-  //componentDidMount or componentDidUpdate
-  useEffect(() => {
-    if (!auth.authenticate) {
-      dispatch(isUserLoggedIn());
-    }
-    if(auth.authenticate){
-      dispatch(getInitialData());
-    }
-    
-
-  }, [auth.authenticate]);
-
+function App() { 
   return (
     <div className="App">
-      <Switch>
-        <PrivateRoute path="/" exact component={Home} />
-        <PrivateRoute path="/page" component={NewPage} />
-        <PrivateRoute path="/category" component={Category} />
-        <PrivateRoute path="/products" component={Products} />
-        <PrivateRoute path="/orders" component={Orders} />
-        
-
-
-        <Route path="/signin" component={Signin} />
-        <Route path="/signup" component={Signup} />
-      </Switch>
+        <Header />
+        <p>This is app.js</p>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/signin" component={Signin} />
+            <Route path="/signup" component={Signup} />
+          </Switch>
+        </BrowserRouter>
     </div>
   );
 }
